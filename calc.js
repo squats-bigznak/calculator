0fb6d8f10744b5f4efc15cda9aee579b98cb335e
squats-bigznak <88125284+squats-bigznak@users.noreply.github.com>
2023-03-18 10:25:07 -0700

    Add pseudocode sketch to js

---
+++ calc.js
@@ -0,0 +1,18 @@
+//input first operand as string
+//if decimcal is used, only allow once
+//upon use of an operator or evaluator ("=")
+        //cast the string to float 
+        //store number to key of first operand object
+        //store operator as string (do nothing if evaluator)
+//input second operand as string
+//if decimal is used, only allow once
+//upon use of another operator or evaluator ("=")
+        //cast string to float
+        //store number to key of second operand object
+        //run switch case that calls function corresponding to operator
+        //use stored operand floats as arguments to function
+//ouput result to screen
+//store result as first operand
+
+//also implement CE and AC buttons which "clear entry" and "all clear"
+ 
